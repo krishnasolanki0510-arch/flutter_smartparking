187c2f0d173bfa48caaa0486c11f8a26844bd13e
krishnasolanki0510-arch <krishnasolanki0510@gmail.com>
2026-05-20 16:36:51 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screen/splash_screen.dart';
 import 'Screen/fregment.dart';
 
 void main() {
@@ -7,7 +8,7 @@ void main() {
 
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FragmentHolder(),
+      home:SplashScreen(),
     ),
   );
 }
